b1ccc771d70e434e15a75865197cfda755d00ccb
Jeferson391 <jefersongustavo0000@gmail.com>
2025-05-22 20:38:36 +0000

    Completo

--- xadrez.c
+++ xadrez.c
@@ -2,6 +2,66 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+#include <stdio.h>
+
+//=========================== Cavalo =============================//
+
+void moverCavalo(int casas){
+    if  (casas > 1){
+        printf("Cima\n");
+        moverCavalo(casas - 1);
+
+    } else if (casas > 0){
+        printf("Direita\n");
+        moverCavalo(casas - 1);
+    }
+}
+
+int main() {
+    moverCavalo(3);
+    return 0;
+}
+
+//============================ Bispo ==============================//
+
+// void moverBispo(int casas){
+//     if  (casas > 0){
+//         printf("Direita - Cima\n");
+//         moverBispo(casas - 1);
+//     }
+// }
+
+// int main() {
+//     moverBispo(5);
+//     return 0;
+// }
+
+//============================ Rainha ==============================//
+
+// void moverRainha(int casas){
+//     if  (casas > 0){
+//         printf("Esquerda\n");
+//         moverRainha(casas - 1);
+//     }
+// }
+// int main() {
+//     moverRainha(8);
+
+//============================= Torre ===============================//
+
+// void moverTorre(int casas){
+//     if  (casas > 0){
+//         printf("Direita\n");
+//         moverTorre(casas - 1);
+//     }
+// }
+
+// int main() {
+//     moverTorre(5);
+//     return 0;
+// }
+
+
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
