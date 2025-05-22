f9ce26f2c44bf87e1acd354ba79b32ba1fcc0c5a
Jeferson391 <jefersongustavo0000@gmail.com>
2025-05-22 20:34:42 +0000

    cc

--- xadrez.c
+++ xadrez.c
@@ -2,8 +2,6 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-#include <stdio.h>
-
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
